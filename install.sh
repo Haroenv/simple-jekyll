a1262513b1f6f7872706a1aee780e4bb0078e242
Haroen Viaene <hello@haroen.me>
2016-02-04 16:28:54 +0100

    colourify

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-echo 'cloning from https://github.com/Haroenv/simple-jekyll.git .'
+# colours
+green="\033[0;32m"
+NC="\033[0m"
+
+echo -e "cloning from ${green}https://github.com/Haroenv/simple-jekyll.git${NC}"
 git clone https://github.com/Haroenv/simple-jekyll.git .
-echo 'moving files'
+echo -e "moving files"
 mv files/* .
-echo 'cleaning up'
+echo -e "cleaning up"
 rm -rf .git files README.md install.sh
-echo 'initialising git'
+echo -e "initialising ${green}git${NC}"
 git init
 git add .
-git commit -m 'initial commit'
+git commit -m "initial commit"
